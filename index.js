593132f4549383e06495b0e2fbf8353061c55590
Noah19-cell <103570425+Noah19-cell@users.noreply.github.com>
2024-10-08 07:55:01 +0200

    finished navigation.

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-//Method 1
+
 const nav = document.querySelector(".primary-navigation");
 const navToggle = document.querySelector(".mobile-nav-toggle");
 
 navToggle.addEventListener("click", () => {
-  nav.classList.toggle("visible");
+    nav.classList.toggle("visible");
+    navToggle.classList.toggle("nav-open");
 })
 
 
